f4fe7c71fe8dbd1e4a1cad13fc7442fc2a51223a
userName <userEmail>
2024-10-10 10:44:01 +0800

    第三方框架导入修正

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-createApp(App).mount('#app')
+
+// ElementPlus
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+
+const app = createApp(App);
+app.use(ElementPlus);
+app.mount('#app');
